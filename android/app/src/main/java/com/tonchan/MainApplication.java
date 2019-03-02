af49684e69206a215a8b36d434a64adef72c5ead
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-03-02 19:43:55 +0000

    Upgrade ssl stuph allowing < android 4 to make https requests

--- android/app/src/main/java/com/tonchan/MainApplication.java
+++ android/app/src/main/java/com/tonchan/MainApplication.java
@@ -1,26 +1,41 @@
 package com.tonchan;
 
 import android.app.Application;
+import android.content.Intent;
 
-import com.facebook.react.ReactApplication;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.horcrux.svg.SvgPackage;
-import com.oblador.keychain.KeychainPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.peel.react.TcpSocketsModule;
-import io.realm.react.RealmReactPackage;
-import com.tradle.react.UdpSocketsModule;
 import com.bitgo.randombytes.RandomBytesPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.security.ProviderInstaller;
+import com.google.android.gms.security.ProviderInstaller.ProviderInstallListener;
+
+import com.horcrux.svg.SvgPackage;
+
+import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
+
+import com.oblador.keychain.KeychainPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+
+import com.peel.react.TcpSocketsModule;
+
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+
+import com.tradle.react.UdpSocketsModule;
+
+import io.realm.react.RealmReactPackage;
+
 import java.util.Arrays;
 import java.util.List;
+
+import org.reactnative.camera.RNCameraPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -63,6 +78,20 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    upgradeSecurityProvider();
     SoLoader.init(this, /* native exopackage */ false);
+  }
+
+  private void upgradeSecurityProvider() {
+    ProviderInstaller.installIfNeededAsync(this, new ProviderInstallListener() {
+      @Override
+      public void onProviderInstalled() {
+      }
+
+      @Override
+      public void onProviderInstallFailed(int errorCode, Intent recoveryIntent) {
+        GooglePlayServicesUtil.showErrorNotification(errorCode, MainApplication.this);
+      }
+    });
   }
 }
