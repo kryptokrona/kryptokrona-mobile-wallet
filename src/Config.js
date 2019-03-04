f0c2dd19dbb795536f833153ba83d9ed51b0761b
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-03-04 18:19:35 +0000

    Version bump

--- src/Config.js
+++ src/Config.js
@@ -230,7 +230,7 @@ const Config = {
     /**
      * Displayed in the settings screen
      */
-    appVersion: 'v0.0.4',
+    appVersion: 'v0.0.5',
 
     /**
      * Base URL for us to chuck a hash on the end, and find a transaction
