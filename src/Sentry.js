54732fcd5dc4948de84caa39e666d89721dadfb0
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-04-11 23:03:18 +0100

    Shuffle outdated comment

--- src/Sentry.js
+++ src/Sentry.js
@@ -6,6 +6,9 @@ import { Sentry } from 'react-native-sentry';
 
 import Config from './Config';
 
+/* Manually comparing to TurtleCoin to try and prevent getting errors reported
+   for forks... */
+/* DO NOT CHANGE THIS LINE WITHOUT ALSO ALTERING THE Sentry.config() LINE - See readme and sentry docs. */
 const sentryIsEnabled = !__DEV__ && Config.coinName === 'TurtleCoin';
 
 export function reportCaughtException(err) {
@@ -18,10 +21,8 @@ export function reportCaughtException(err) {
 }
 
 export function initSentry() {
-    /* CHANGE THIS IF YOU ARE FORKING! */
-    /* Manually comparing to TurtleCoin to try and prevent getting errors reported
-       for forks... */
     if (sentryIsEnabled) {
+        /* CHANGE THIS IF YOU ARE FORKING! */
         Sentry.config('https://8ecf138e1d1e4d558178be3f2b5e1925@sentry.io/1411753').install();
         Sentry.setVersion(Config.appVersion);
     }
