de5e6fcdf8ee32b97214e71bf01c79cdbf253ea4
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-07-30 01:51:00 +0100

    Fix sentry not being activated

--- android/app/src/main/java/com/tonchan/MainApplication.java
+++ android/app/src/main/java/com/tonchan/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
